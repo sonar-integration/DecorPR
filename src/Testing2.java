d4c54eadf07da8f9c8e3d971d6ddfe90dd140a96
revanth <xyz@xyz.com>
2019-02-06 16:36:57 +0530

    Added Testing2.java file

---
+++ src/Testing2.java
@@ -0,0 +1,11 @@
+
+public class Testing2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing2");
+		System.out.println("hello");
+		System.out.println("how do you do");
+	}
+
+}
